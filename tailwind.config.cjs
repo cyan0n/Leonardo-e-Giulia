4fb0ca9f65cdb74f43367255837dc99d1bba82a2
Cyan0n <cyan0n.dev@gmail.com>
2023-01-22 20:16:20 +0100

    Update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,35 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        blue: {
+          print: "#304faa",
+        },
+        green: {
+          print: "#2d9d4c",
+        },
+        "dark-green": {
+          print: "#1f683a",
+        },
+        lime: {
+          print: "#b3dc18",
+        },
+        orange: {
+          print: "#fbc108",
+        },
+        pink: {
+          print: "#ef2891",
+        },
+        red: {
+          print: "#ee2332",
+        },
+        sky: {
+          print: "#6bd0f6",
+        },
+        yellow: {
+          print: "#eeef2a",
+        },
+      },
       fontFamily: {
         sans: ["Yanone Kaffeesatz", ...defaultTheme.fontFamily.sans],
       },
